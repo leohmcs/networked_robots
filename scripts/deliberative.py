82e494be48ac133dc66759455eef3244c0e696e3
leohmcs <leohmcs@gmail.com>
2021-12-21 14:51:46 -0300

    Remove unused nodes.

--- scripts/deliberative.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#coding: utf-8
-
-class Deliberative:
-    def __init__(self):
-        pass
-
--- scripts/utils/transformations.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# rotation around z-axis
-def Rz(x, y, z, th):
-    mat = np.array([[np.cos(th), np.sin(th), 0], [np.cos(th), -np.sin(th), 0], [0, 0, 1]])
-    p = np.array([x, y, z])
-
-    return np.dot(mat, p)
